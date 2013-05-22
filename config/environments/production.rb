43f687a598c13ad897303abb457fe802fb517968
Tharindu Denuwara <ztraightline@gmail.com>
2013-05-22 21:45:27 +1200

    Precompiled all css files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ SampleApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Precompile ALL assets
+  config.assets.precompile << '*.css'
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
